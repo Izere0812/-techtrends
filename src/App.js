749cd832e4422307065f728d4173a500f6cb9df5
Izere0812 <aimechris0812@gmail.com>
2025-05-08 15:13:34 +0200

    Updated Footer with scroll-to-top and social icons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-// src/App.js
+// App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
-
 import './assets/style.css';
 
-const App = () => {
+function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
+    <div id="root">
+      <Router>
+        <Navbar />
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
+    </div>
   );
-};
+}
 
 export default App;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,26 @@
 // components/Footer.js
 import React from 'react';
+import { FaFacebookF, FaTwitter, FaInstagram, FaArrowUp } from 'react-icons/fa';
+import '../assets/style.css';
 
 function Footer() {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
   return (
-    <footer>
+    <footer className="site-footer">
+      <div className="social-icons">
+        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer"><FaFacebookF  /></a>
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
+        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+      </div>
+
       <p>&copy; 2025 TechTrends Ltd. All rights reserved.</p>
+
+      <button className="scroll-top-btn" onClick={scrollToTop}>
+        <FaArrowUp />
+      </button>
     </footer>
   );
 }
